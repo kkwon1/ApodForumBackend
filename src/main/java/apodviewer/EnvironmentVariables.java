7da4c27dc079b0f26b91def78d73c1eb32cac7d1
Kevin Kwon <kevinkwon1995@gmail.com>
2023-01-19 20:25:31 -0800

    Adding token validation against the comments API

--- src/main/java/apodviewer/EnvironmentVariables.java
+++ src/main/java/apodviewer/EnvironmentVariables.java
@@ -7,6 +7,8 @@ public class EnvironmentVariables {
     // Comma separated string of origins
     public static String ALLOWED_ORIGINS;
     public static String NASA_API_KEY;
+    public static String AUTH0_AUDIENCE;
+    public static String JWT_ISSUER;
 
     public static void initializeEnvironmentVariables() {
         MONGO_ENDPOINT = System.getenv("MONGO_ENDPOINT");
@@ -25,6 +27,18 @@ public class EnvironmentVariables {
         if (NASA_API_KEY == null || NASA_API_KEY.isBlank()) {
             Dotenv dotenv = Dotenv.load();
             NASA_API_KEY = dotenv.get("NASA_API_KEY");
+        }
+
+        AUTH0_AUDIENCE = System.getenv("AUTH0_AUDIENCE");
+        if (AUTH0_AUDIENCE == null || AUTH0_AUDIENCE.isBlank()) {
+            Dotenv dotenv = Dotenv.load();
+            AUTH0_AUDIENCE = dotenv.get("AUTH0_AUDIENCE");
+        }
+
+        JWT_ISSUER = System.getenv("JWT_ISSUER");
+        if (JWT_ISSUER == null || JWT_ISSUER.isBlank()) {
+            Dotenv dotenv = Dotenv.load();
+            JWT_ISSUER = dotenv.get("JWT_ISSUER");
         }
     }
 }
---
+++ src/main/java/apodviewer/auth/SecurityConfig.java
@@ -0,0 +1,52 @@
+package apodviewer.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.*;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static apodviewer.EnvironmentVariables.AUTH0_AUDIENCE;
+import static apodviewer.EnvironmentVariables.JWT_ISSUER;
+
+/**
+ * Configures our application with Spring Security to restrict access to our API endpoints.
+ */
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        /*
+        This is where we configure the security required for our endpoints and setup our app to serve as
+        an OAuth2 Resource Server, using JWT validation.
+        */
+        http.authorizeRequests()
+                .mvcMatchers("/apod").permitAll()
+                .mvcMatchers("/post").permitAll()
+                .mvcMatchers("/comments").authenticated()
+                .and().cors()
+                .and().oauth2ResourceServer().jwt();
+        return http.build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        /*
+        By default, Spring Security does not validate the "aud" claim of the token, to ensure that this token is
+        indeed intended for our app. Adding our own validator is easy to do:
+        */
+
+        NimbusJwtDecoder jwtDecoder = JwtDecoders.fromOidcIssuerLocation(JWT_ISSUER);
+
+        OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(AUTH0_AUDIENCE);
+        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(JWT_ISSUER);
+        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
+
+        jwtDecoder.setJwtValidator(withAudience);
+
+        return jwtDecoder;
+    }
+}
